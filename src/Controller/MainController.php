a80188d0ea8e9faf8d15697f6d2b2b1f8f42522a
Gerald Col <gnut@gnut.eu>
2021-03-23 14:56:13 +0100

    derniere modifs

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AnnoncesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(AnnoncesRepository $repo): Response
     {
+        $annonces= $repo->findAll();
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'annonces' => $annonces,
         ]);
     }
     /**
